bb2b09727c689549ba830c4f008cf77856e8efa5
Ubuntu <ubuntu@ip-172-31-20-250.eu-west-1.compute.internal>
2016-01-28 19:44:58 +0000

    Commit before screenshare fix

---
+++ .bash_history
@@ -0,0 +1,505 @@
+ls
+echo "deb http://ubuntu.kurento.org trusty kms6" | sudo tee /etc/apt/sources.list.d/kurento.list
+wget -O - http://ubuntu.kurento.org/kurento.gpg.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install kurento-media-server-6.0
+sudo service kurento-media-server-6.0 start
+sudo service kurento-media-server-6.0 stop
+ls
+cd /etc
+ls
+cd kurento
+ls
+cd modules
+ls
+cd kurento
+ls
+vim WevRtcEndpoint.conf.ini
+ls
+vim WebRtcEndpoint.conf.ini
+vi WebRtcEndpoint.conf.ini
+ls
+cd /etc/kurento
+ls
+
+vim kurento.conf.json
+ls
+cd modules
+ls
+cd kurento
+ls
+vim WebRtcEndpoint.conf.ini
+cd /etc/kurento
+cd modules/kurento
+ls
+vi WebRtcEndpoint.conf.ini
+ls -l
+sudo vi WebRtcEndpoint.conf.ini
+vi WebRtcEndpoint.conf.ini
+sudo vi WebRtcEndpoint.conf.ini
+vi WebRtcEndpoint.conf.ini
+sudo service krurento-media-server restart
+sudo service kurento-media-server restart
+cd ..
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+sudo npm install -g bower
+git clone https://github.com/Kurento/kurento-tutorial-node.git
+sudo apt-get install git
+git clone https://github.com/Kurento/kurento-tutorial-node.git
+cd etc
+git clone https://github.com/Kurento/kurento-tutorial-node.git
+ls
+cd ..
+cd home
+ls
+cd ubuntu
+ls
+git clone https://github.com/Kurento/kurento-tutorial-node.git
+cd kurento-tutorial-node/kurento-one2many-call
+git checkout 6.2.2-dev
+npm install
+npm start
+sudo service kurento-media-server-6.0 start
+npm start
+cd
+sudo apt-get remove kurento-6.0
+sudo apt-get remove kurento-media-server-6.0
+sudo apt-update
+sudo apt-get update
+sudo apt-get install kurento-media-server-6.2
+
+ls
+sudo apt-get install kurento-media-server-6.0
+cd kurento-tutorial-node
+ls
+cd kurento-one2many-call
+ls
+sudo service kurento-media-server-6.2.2 start
+sudo service kurento-media-server-6.0 start
+npm start
+vi server.js
+vi npm-debug.log
+cd
+cd /etc/kurento
+ls
+cd modules/kurento/
+ks
+ls
+cd ..
+ls
+cd ..
+ls
+vi sdp_pattern.txt
+vi kurento.conf.json
+ls
+lsof -i :8080
+lsof
+lsof -i 8080
+netstat -tulno | grep 8080
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-one2many-call
+ls
+npm install
+sudo service kurento-media-server-6.0 start
+npm start
+ls
+cd kurento-tutorial-node
+ls
+kurento-one2many-call
+cd kurento-one2many-call
+ls
+sudo service kurento-media-server-6.0 start
+rm -r node_modules
+npm cache clean
+sudo npm cache clean
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+sudo npm install -g bower
+npm install
+sudo npm install npm -g
+npm start -- --ws_uri=ws://52.49.81.164:8443/
+cd kurento-tutorial-node
+cd kurento-one2many-call
+netstat
+netstat -l
+cd kurento-tutorial-node
+ls
+cd kurento-hello-world
+ls
+npm start
+cd kurento-module-node
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-one2many-call
+sudo vi server.js
+npm start
+ps -ax | grep node
+kill 3028
+kill 3029
+kill 3184
+npm start
+cd kurento
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-hello-world
+ls
+sudo vi server.js
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+sudo npm install -g bower
+git checkout 6.2.2-dev
+npm install
+rm -r node_modules
+npm install
+npm cache clean
+rm -r node_modules
+npm cache clean
+npm install
+sudo service kurento-media-server-6.0 stop
+sudo service kurento-media-server-6.0 start
+npm start
+git co 6.2.1
+ls
+cd node_modules
+ls
+cd
+.. cd
+cd
+cd /kurento-tutorial-node/kurento-hello-world
+cd /kurento-tutorial-node/kurento-hello-world/
+cd /kurento-tutorial-node/
+cd /kurento-tutorial-node
+ls
+cd kurento-tutorial-node
+cd kurento-hello-world
+ls
+cd keys
+ls
+cd ..
+cd static
+ls
+cd bower_components
+ls
+cd ..
+clear
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-tutorial-node
+ls
+git --version
+ls
+clear
+git clone -b branch --single-branch git://github/repository.git
+git clone -b develop --single-branch git://github.com/Kurento/kurento-tutorial-js/tree/develop/kurento-recorder-screen.git
+git clone -b develop --single-branch git://github.com/Kurento/kurento-tutorial-js.git
+git clone -b develop --single-branch git://github.com/Kurento/kurento-tutorial-js.git develop
+ks
+ls
+cd develop
+ls
+cd kurento-recorder-screen
+ls
+npm install
+npm start
+clear
+ls
+sudo vi server.js
+npm start
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-one2many-call
+npm start
+ls
+dc kurento-tutorial-node
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-one2many-call
+ls
+npm start
+cd static
+ls
+sudo vi index2.html
+cd ..
+npm start
+ls
+cd Project
+ls
+mkdir Temporary
+ls
+cd temporary
+cd Temporary
+ls
+git clone https://github.com/Kurento/kurento-tutorial-node.git
+cd ..
+ls
+cd kurento-one2many-cal
+cd kurento-one2many-call
+ls
+sudo vi server.js
+npm install
+npm start
+ls
+cd static
+ls
+sudo vi index.html
+cd..
+cd ..
+ls
+npm start
+ls
+cd project
+cd Project
+ls
+cd kurento-one2many-call
+ls
+npm start
+cd project
+cd Project
+ls
+cd kurento-one2many-call
+ls
+cd static
+ls
+sudo vi getScreenId.html
+cd ..
+ls
+npm start
+cd static
+ls
+sudo vi index.html
+cd ..
+npm start
+cd Project
+ls
+cd kurento-one2many-call
+ls
+npm start
+ls
+cd static
+ls
+sudo vi getScreenId.js
+cd ..
+npm start
+ls
+cd static
+ls
+sudo vi getScreenId.js
+sudo vi index.html
+cd ..
+ls
+npm start
+ls
+cd Project
+ls
+cd kurento-one2many-call
+ls
+cd static
+ls
+sudo vi getScreenId.js
+cd ..
+ls
+npm start
+ls
+npm start
+sudo vi server.js
+ls
+npm start
+sudo vi npm-debug.log
+clear
+npm start
+npm stop
+npm kill
+npm start
+ps aux|grep npm
+kill 9515
+kill 9991
+ls
+npm start
+netstat grep|8080
+netsat 8080
+netstat
+ps aux|grep npm
+npm start
+cd Project
+cd kurento-one2many-call
+npm start
+cd ..
+;s
+cd ..
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-one2many-call
+ls
+npm start
+ps aux|grep npm
+kill 10118
+cd ..
+cd Project
+ls
+cd kurento-one2many-call
+ls
+npm start
+npm cache clean
+npm start
+npm cache clean
+ps aux|grep npm
+kill 10152
+cd Project
+ls
+cd kurento-one2many-call
+ls
+npm start
+ps aux|grep npm
+kill 1320
+npm start
+cd Project
+ls
+cd Temporary
+ls
+cd ..
+mkdir test
+cd test
+cd ..
+cd Project
+ls
+cd kurento-one2many-call
+ls
+npm start
+ls
+sudo vi server.js
+npm start
+cd .. 
+cd ..
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-one2many-call
+ls
+npm start
+sudo vi server.js
+clear
+ls
+sudo vi package.json
+ls static
+cd js
+ls
+cd static
+cd js
+ls
+cd ..
+cd etc
+cd /etc
+ls
+cd kurento
+ls
+cd modules
+ls
+cd kurento
+ls
+sudo vi WebRtcEndpoint.conf.ini
+ls
+cd Project
+sudo service kurento-media-server-6.0 stop
+sudo service kurento-media-server-6.0 start
+cd kurento-one2many-call
+ls
+npm start
+clear
+ls
+cd Project
+ls
+cd kurento-one2many-call
+ls
+cd static
+ls
+sudo vi index.html
+cd ..
+npm start
+ls
+ps aux|grep npm
+kill 2505
+ls
+npm start
+sudo service kurento-media-server-6.0 stop
+sudo service kurento-media-server-6.0 start
+ls
+cd Project
+ls
+cd kurento-one2many-call
+ls
+npm start
+cd Project
+cd kurento-one2many-call
+npm start
+clear
+sudo service kurento-media-server-6.0 stop
+sudo service kurento-media-server-6.0 start
+npm start
+ls
+cd Project
+ls
+cd kurento-one2many-call
+ls
+sudo service kurento-media-server-6.0 stop
+sudo service kurento-media-server-6.0 start
+npm start
+cd Project
+ls
+cd kurento-one2many-call
+ls
+npm start
+ps aux|grep npm
+ls
+kill 1534
+;s
+ls
+clear
+npm start
+cd Project
+ls
+cd kurento-one2many-call
+ls
+npm start
+cd ..
+ls
+cd ..
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-one2many-call
+ls
+npm start
+cd Porject
+cd Project
+cd kurento-one2many-call
+ls
+sudo service kurento-media-server-6.0 stop
+sudo service kurento-media-server-6.0 start
+npm start
+cd ..
+ls
+cd ..
+ls
+cd kurento-tutorial-node
+ls
+cd kurento-one2many-call
+ls
+npm start
+clear
+npm start
+sudo service kurento-media-server-6.0 stop
+sudo service kurento-media-server-6.0 start
+cd Project
+ls
+cd kurento-one2many-call
+ls
+npm start
---
+++ Project/kurento-one2many-call/server.js
@@ -0,0 +1,384 @@
+/*
+ * (C) Copyright 2014-2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+var path = require('path');
+var url = require('url');
+var express = require('express');
+var minimist = require('minimist');
+var ws = require('ws');
+var kurento = require('kurento-client');
+var fs    = require('fs');
+var https = require('https');
+
+//sets kurento client url 8888, kurento client running on port 8888, and access url both on port 8443 
+var argv = minimist(process.argv.slice(2), {
+    default: {
+        as_uri: 'https://52.49.81.164:8443/',
+        ws_uri: 'ws://52.49.81.164:8888/kurento'
+    }
+});
+
+//https certs
+var options =
+{
+  key:  fs.readFileSync('keys/server.key'),
+  cert: fs.readFileSync('keys/server.crt')
+};
+//express framework
+var app = express();
+
+/*
+ * Definition of global variables.
+ */
+var idCounter = 0;
+var candidatesQueue = {};
+var kurentoClient = null;
+var presenter = null;
+var viewers = [];
+var noPresenterMessage = 'No active presenter. Try again later...';
+
+/*
+ * Server startup
+ */
+ //parsing url
+var asUrl = url.parse(argv.as_uri);
+//selectig port from url
+var port = asUrl.port;
+//creating https server on port
+var server = https.createServer(options, app).listen(port, function() {
+    console.log('Kurento Tutorial started');
+    console.log('Open ' + url.format(asUrl) + ' with a WebRTC capable browser');
+});
+
+// new websocket server
+var wss = new ws.Server({
+    server : server,
+    path : '/one2many'
+});
+
+//creating session id for new viewer clients
+function nextUniqueId() {
+	idCounter++;
+	return idCounter.toString();
+}
+
+/*
+ * Management of WebSocket messages
+ */
+wss.on('connection', function(ws) {
+	//creates a session id
+	var sessionId = nextUniqueId();
+	console.log('Connection received with sessionId ' + sessionId);
+	//logging errors 
+    ws.on('error', function(error) {
+        console.log('Connection ' + sessionId + ' error');
+        stop(sessionId);
+    });
+	//closing websocket
+    ws.on('close', function() {
+        console.log('Connection ' + sessionId + ' closed');
+        stop(sessionId);
+    });
+
+    //message processing
+    ws.on('message', function(_message) {
+        var message = JSON.parse(_message);
+        console.log('Connection ' + sessionId + ' received message ', message);
+        //dealing with messages passed on websocket
+        switch (message.id) {
+        case 'presenter':
+			startPresenter(sessionId, ws, message.sdpOffer, function(error, sdpAnswer) { // starts presenter by sending sessionID, websocket and sdpoffer
+				if (error) {
+					return ws.send(JSON.stringify({
+						id : 'presenterResponse',
+						response : 'rejected',
+						message : error
+					}));
+				}
+				ws.send(JSON.stringify({
+					id : 'presenterResponse',
+					response : 'accepted',
+					sdpAnswer : sdpAnswer
+				}));
+			});
+			break;
+
+        case 'viewer':
+			startViewer(sessionId, ws, message.sdpOffer, function(error, sdpAnswer) {
+				if (error) {
+					return ws.send(JSON.stringify({
+						id : 'viewerResponse',
+						response : 'rejected',
+						message : error
+					}));
+				}
+
+				ws.send(JSON.stringify({
+					id : 'viewerResponse',
+					response : 'accepted',
+					sdpAnswer : sdpAnswer
+				}));
+			});
+			break;
+
+        case 'stop':
+            stop(sessionId);
+            break;
+
+        case 'onIceCandidate':
+            onIceCandidate(sessionId, message.candidate);
+            break;
+
+        default:
+            ws.send(JSON.stringify({
+                id : 'error',
+                message : 'Invalid message ' + message
+            }));
+            break;
+        }
+    });
+});
+
+/*
+ * Definition of functions
+ */
+
+// Recover kurentoClient for the first time.
+function getKurentoClient(callback) {
+    if (kurentoClient !== null) {
+        return callback(null, kurentoClient);
+    }
+
+    kurento(argv.ws_uri, function(error, _kurentoClient) { //function that is part of instanciating a kurento client
+        if (error) {
+            console.log("Could not find media server at address " + argv.ws_uri);
+            return callback("Could not find media server at address" + argv.ws_uri
+                    + ". Exiting with error " + error);
+        }
+
+        kurentoClient = _kurentoClient;
+        callback(null, kurentoClient);
+    });
+}
+
+function startPresenter(sessionId, ws, sdpOffer, callback) { 
+	clearCandidatesQueue(sessionId);
+
+	if (presenter !== null) {
+		stop(sessionId);
+		return callback("Another user is currently acting as presenter. Try again later ...");
+	}
+
+	presenter = {
+		id : sessionId,
+		pipeline : null,
+		webRtcEndpoint : null
+	}
+
+	getKurentoClient(function(error, kurentoClient) {
+		if (error) {
+			stop(sessionId);
+			return callback(error);
+		}
+
+		if (presenter === null) {
+			stop(sessionId);
+			return callback(noPresenterMessage);
+		}
+
+		kurentoClient.create('MediaPipeline', function(error, pipeline) {
+			if (error) {
+				stop(sessionId);
+				return callback(error);
+			}
+
+			if (presenter === null) {
+				stop(sessionId);
+				return callback(noPresenterMessage);
+			}
+
+			presenter.pipeline = pipeline;
+			pipeline.create('WebRtcEndpoint', function(error, webRtcEndpoint) {
+				if (error) {
+					stop(sessionId);
+					return callback(error);
+				}
+
+				if (presenter === null) {
+					stop(sessionId);
+					return callback(noPresenterMessage);
+				}
+
+				presenter.webRtcEndpoint = webRtcEndpoint;
+
+                if (candidatesQueue[sessionId]) {
+                    while(candidatesQueue[sessionId].length) {
+                        var candidate = candidatesQueue[sessionId].shift();
+                        webRtcEndpoint.addIceCandidate(candidate);
+                    }
+                }
+
+                webRtcEndpoint.on('OnIceCandidate', function(event) {
+                    var candidate = kurento.register.complexTypes.IceCandidate(event.candidate);
+                    ws.send(JSON.stringify({
+                        id : 'iceCandidate',
+                        candidate : candidate
+                    }));
+                });
+
+				webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer) {
+					if (error) {
+						stop(sessionId);
+						return callback(error);
+					}
+
+					if (presenter === null) {
+						stop(sessionId);
+						return callback(noPresenterMessage);
+					}
+
+					callback(null, sdpAnswer);
+				});
+
+                webRtcEndpoint.gatherCandidates(function(error) {
+                    if (error) {
+                        stop(sessionId);
+                        return callback(error);
+                    }
+                });
+            });
+        });
+	});
+}
+
+function startViewer(sessionId, ws, sdpOffer, callback) {
+	clearCandidatesQueue(sessionId);
+
+	if (presenter === null) {
+		stop(sessionId);
+		return callback(noPresenterMessage);
+	}
+
+	presenter.pipeline.create('WebRtcEndpoint', function(error, webRtcEndpoint) {
+		if (error) {
+			stop(sessionId);
+			return callback(error);
+		}
+		viewers[sessionId] = {
+			"webRtcEndpoint" : webRtcEndpoint,
+			"ws" : ws
+		}
+
+		if (presenter === null) {
+			stop(sessionId);
+			return callback(noPresenterMessage);
+		}
+
+		if (candidatesQueue[sessionId]) {
+			while(candidatesQueue[sessionId].length) {
+				var candidate = candidatesQueue[sessionId].shift();
+				webRtcEndpoint.addIceCandidate(candidate);
+			}
+		}
+
+        webRtcEndpoint.on('OnIceCandidate', function(event) {
+            var candidate = kurento.register.complexTypes.IceCandidate(event.candidate);
+            ws.send(JSON.stringify({
+                id : 'iceCandidate',
+                candidate : candidate
+            }));
+        });
+
+		webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer) {
+			if (error) {
+				stop(sessionId);
+				return callback(error);
+			}
+			if (presenter === null) {
+				stop(sessionId);
+				return callback(noPresenterMessage);
+			}
+
+			presenter.webRtcEndpoint.connect(webRtcEndpoint, function(error) {
+				if (error) {
+					stop(sessionId);
+					return callback(error);
+				}
+				if (presenter === null) {
+					stop(sessionId);
+					return callback(noPresenterMessage);
+				}
+
+				callback(null, sdpAnswer);
+		        webRtcEndpoint.gatherCandidates(function(error) {
+		            if (error) {
+			            stop(sessionId);
+			            return callback(error);
+		            }
+		        });
+		    });
+	    });
+	});
+}
+
+function clearCandidatesQueue(sessionId) {
+	if (candidatesQueue[sessionId]) {
+		delete candidatesQueue[sessionId];
+	}
+}
+
+function stop(sessionId) {
+	if (presenter !== null && presenter.id == sessionId) {
+		for (var i in viewers) {
+			var viewer = viewers[i];
+			if (viewer.ws) {
+				viewer.ws.send(JSON.stringify({
+					id : 'stopCommunication'
+				}));
+			}
+		}
+		presenter.pipeline.release();
+		presenter = null;
+		viewers = [];
+
+	} else if (viewers[sessionId]) {
+		viewers[sessionId].webRtcEndpoint.release();
+		delete viewers[sessionId];
+	}
+
+	clearCandidatesQueue(sessionId);
+}
+
+function onIceCandidate(sessionId, _candidate) {
+    var candidate = kurento.register.complexTypes.IceCandidate(_candidate);
+
+    if (presenter && presenter.id === sessionId && presenter.webRtcEndpoint) {
+        console.info('Sending presenter candidate');
+        presenter.webRtcEndpoint.addIceCandidate(candidate);
+    }
+    else if (viewers[sessionId] && viewers[sessionId].webRtcEndpoint) {
+        console.info('Sending viewer candidate');
+        viewers[sessionId].webRtcEndpoint.addIceCandidate(candidate);
+    }
+    else {
+        console.info('Queueing candidate');
+        if (!candidatesQueue[sessionId]) {
+            candidatesQueue[sessionId] = [];
+        }
+        candidatesQueue[sessionId].push(candidate);
+    }
+}
+
+app.use(express.static(path.join(__dirname, 'static')));
